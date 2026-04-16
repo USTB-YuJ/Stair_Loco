7fe6101807e8e0bf86966e90d145b96421b17fa0
yuj <1312711820@qq.com>
2026-04-16 10:48:40 +0800

    bash 文件更新到适配h1

--- TienKung-Lab/play.sh
+++ TienKung-Lab/play.sh
@@ -1,5 +1,5 @@
 python legged_lab/scripts/play.py  \
-    --task=h1_2_dwaq \
+    --task=h1_dwaq \
     --num_envs=10 \
-    --load_run=2026-04-14_20-38-38 \
-    --checkpoint=model_35000.pt \
+    --load_run=2026-04-15_19-30-33 \
+    --checkpoint=model_10000.pt \
--- TienKung-Lab/sim2sim.sh
+++ TienKung-Lab/sim2sim.sh
@@ -1,4 +1,3 @@
-python legged_lab/scripts/sim2sim_h1_2_dwaq.py \
-  --checkpoint logs/h1_2_dwaq/2026-04-14_20-38-38/model_35000.pt \
-  --model legged_lab/assets/h1_2_description/h1_2_handless.xml \
-  --duration 100
+python legged_lab/scripts/sim2sim_h1_dwaq.py \
+  --checkpoint /home/eai/workspace/Stair_Loco/TienKung-Lab/logs/h1_dwaq/2026-04-15_19-30-33/model_10000.pt \
+  --model /home/eai/workspace/Stair_Loco/TienKung-Lab/legged_lab/assets/h1_description/mjcf/scene.xml \
